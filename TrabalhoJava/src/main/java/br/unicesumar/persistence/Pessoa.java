c1845c969af3144ef558775957e1f810e84324c1
Washington Junior <jesus__vs@hotmail.com>
2013-11-09 20:08:02 -0200

    Pessoa adicionada

--- TrabalhoJava/src/main/java/br/unicesumar/persistence/Pessoa.java
+++ TrabalhoJava/src/main/java/br/unicesumar/persistence/Pessoa.java
@@ -1,5 +1,29 @@
 package br.unicesumar.persistence;
 
-public class Pessoa {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import br.unicesumar.types.Sexo;
+
+
+@Entity
+public class Pessoa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false)
+	private int idade;
+
+	@Column(nullable = false)
+	private Sexo sexo;
+	
+	@Column(nullable = false)
+	private String cpf;
 }
